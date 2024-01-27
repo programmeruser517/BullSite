9bf8189a22ff0982ef6d0d2b24a1f0f76887a718
Dominick Schwutke <schwutk2@msu.edu>
2024-01-27 17:59:37 -0500

    initial popup.js addons

--- popup.js
+++ popup.js
@@ -1,4 +1,24 @@
 //what to do when the button is clicked
-document.getElementById('clickMe').addEventListener('click', function() {
-    alert('Button clicked!');
-  });  
+let isOn = false;
+  //creates vaiable to tell if the button is on or not.
+  
+document.addEventListener('DOMContentLoaded', function () {
+  const toggleButton = document.getElementById('toggleButton');
+  toggleButton.addEventListener('click', function () {
+    isOn = !isOn;
+    //once the button is clicked, this variable is changed to the opposite of whatever it was before.
+    updateToggleButton();
+    //Update the button using the function provided below
+  });
+
+  // Initialize the button state
+  updateToggleButton();
+});
+
+function updateToggleButton() {
+  const toggleButton = document.getElementById('toggleButton');
+  //Element toggleButton can be found in the popup.html file. This file contains the dimensions aswell as the logo PNG for the button.
+  toggleButton.className = isOn ? 'on' : 'off';
+}
+  //This code updates the ID element of the on and off code, making it switch to our logo
+  //there might need to be a 
